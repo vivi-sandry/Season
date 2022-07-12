83c9656928c070ebde3536e3e49622577321b6c4
vivi-sandry <72271002+vivi-sandry@users.noreply.github.com>
2022-07-12 16:53:04 -0300

    criacao da tela welcome.

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/season', function () {
     return view('welcome');
 });
 //grupo de rotas referentes as telas de Alunos
